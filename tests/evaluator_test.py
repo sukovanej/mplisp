75e46459afee693b902c1e4bb60b54074c93abf8
sukovanej <suk@eurosat.cz>
2018-02-05 13:30:10 +0100

    string evaluation test

--- tests/evaluator_test.py
+++ tests/evaluator_test.py
@@ -9,11 +9,14 @@ class TestEvaluator(unittest.TestCase):
         input1 = '\'str\''
         input2 = '\"str\"'
         input3 = '(def s \'ahoj\')\n(== s \'ahoj\')'
+        input4 = '(def s \'ahoj\')\ns'
 
         output1 = list(evaluator.evaluate(input1))
         output2 = list(evaluator.evaluate(input2))
         output3 = list(evaluator.evaluate(input3))
+        output4 = list(evaluator.evaluate(input4))
 
         self.assertEqual(output1, ['str'])
         self.assertEqual(output2, ['str'])
         self.assertEqual(output3, [None, True])
+        self.assertEqual(output4, [None, 'ahoj'])
